c003efd1bab875751971893af6e11433cd1fc156
Kent <767188747@qq.com>
2018-06-17 17:27:55 +0800

    4th

--- 4th/ticket_selling.py
+++ 4th/ticket_selling.py
@@ -1,2 +1,34 @@
 # 4个售票窗口同时出售30张电影票
 
+import threading
+import time
+
+
+class BoxOffice(threading.Thread):
+    def __init__(self, num):
+        threading.Thread.__init__(self)
+        self.setName('Window ' + str(num))  # 售票窗口号
+
+    def run(self):
+        global ticket
+        global lock
+
+        while ticket > 0:
+            lock.acquire()  # 加锁
+            print(self.getName() + ' sells ticket ' + str(ticket))
+            ticket = ticket - 1  # 票数减1
+            lock.release()  # 解锁
+            time.sleep(0.1)  # 每次售票完毕后休眠100ms
+
+
+if __name__ == '__main__':
+    ticket = 30  # 共30张电影票
+
+    lock = threading.Lock()  # 创建锁
+
+    threads = []
+    for i in range(4):  # 创建4个线程
+        threads.append(BoxOffice(i + 1))
+
+    for t in threads:
+        t.start()
--- 4th/withdraw_money.py
+++ 4th/withdraw_money.py
@@ -1,2 +1,53 @@
 # 张三、李四两个人，使用同一个账户，张三在柜台取钱，李四在ATM取钱
 
+import threading
+import time
+
+
+class Bank:
+    def __init__(self):
+        self.money = 10000  # 初始10000元
+        self.lock = threading.Lock()  # 创建锁
+
+    def counter_withdraw(self, money1):
+        self.lock.acquire()  # 加锁
+        self.money = self.money - money1
+        print('ZhangSan gets $' + str(money1) + ' from counter, and remains $' + str(self.money) + ' in the account')
+        self.lock.release()  # 解锁
+
+    def atm_withdraw(self, money2):
+        self.lock.acquire()  # 加锁
+        self.money = self.money - money2
+        print('LiSi gets $' + str(money2) + ' from ATM, and remains $' + str(self.money) + ' in the account')
+        self.lock.release()  # 解锁
+
+
+class ZhangSan(threading.Thread):
+    def __init__(self, same_account):  # 初始化为同一账户
+        threading.Thread.__init__(self)
+        self.bank = same_account
+
+    def run(self):
+        while self.bank.money >= 100:
+            self.bank.counter_withdraw(100)  # 柜台取钱100
+            time.sleep(0.05)
+
+
+class LiSi(threading.Thread):
+    def __init__(self, same_account):  # 初始化为同一账户
+        threading.Thread.__init__(self)
+        self.bank = same_account
+
+    def run(self):
+        while self.bank.money >= 300:
+            self.bank.atm_withdraw(300)  # ATM取钱300
+            time.sleep(0.1)
+
+
+if __name__ == '__main__':
+    account = Bank()
+
+    p1 = ZhangSan(account)
+    p2 = LiSi(account)
+    p1.start()
+    p2.start()
